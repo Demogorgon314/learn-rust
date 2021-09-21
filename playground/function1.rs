e64bc75bbbac78e351c6459fbcb61beb0b99c363
Demogorgon314 <wangkai744567028@gmail.com>
2021-09-21 17:00:03 +0800

    Add playground

---
+++ playground/function1.rs
@@ -0,0 +1,17 @@
+// 函数是一等公民，可以作为参数或者返回值
+fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
+    f(value)
+}
+
+fn square(value: i32) -> i32 {
+    value * value
+}
+
+fn cube(value: i32) -> i32 {
+    value * value * value
+}
+
+fn main() {
+    println!("apply square: {}", apply(2, square));
+    println!("apply cube: {}", apply(2, cube));
+}
